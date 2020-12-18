c6223564e5d633ab2a23bb27b5dedffec5930833
Andrew <andrebobrovs@mail.ru>
2020-12-18 22:50:00 +0700

    writing WorkflowParser: workflow block parsing is written

--- lab2/WorkflowParser.h
+++ lab2/WorkflowParser.h
@@ -1,9 +1,30 @@
 #pragma once
 #include "Parser.h"
+#include <string>
+#include <iostream>
+
+enum {max_arg_number = 10};
+
+namespace WorkflowParserException {
+	static enum {type_number = 3};
+	static const string Types[type_number] = {"FileOpen", "BlockSyntax", "PipeSyntax"};
+}
 
 class WorkflowParser : public Parser{
+	class ParserException {
+	private:
+		string type;
+		string message;
+	public:
+		void print_exception();
+		ParserException(string t, string m);
+	};
+
 private:
+	pair<int, Node> parse_block_line(string str);
+	// parse nodes method
 	unordered_map<int, Node>* const Parser::parse_nodes(ifstream* input) override;
+	// parse workflow pipeline method
 	list<int>* const Parser::parse_pipe(ifstream* input) override;
 public:
 	// main method
